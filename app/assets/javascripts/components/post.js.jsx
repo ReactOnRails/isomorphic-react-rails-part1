4fd457fcd7a9f1b8c8ec823342f00722c4848345
Olivier Lance <olivier@digitalcuisine.fr>
2014-08-27 23:48:34 +0200

    Components for a single post

---
+++ app/assets/javascripts/components/post.js.jsx
@@ -0,0 +1,35 @@
+/** @jsx React.DOM **/
+
+var Post = React.createClass({
+    render: function() {
+        return (
+            <div className="post">
+                <PostHeader post={this.props.post} />
+                <PostContent post={this.props.post} />
+            </div>
+        );
+    }
+});
+
+var PostHeader = React.createClass({
+    render: function() {
+        return (
+            <div className="post-header">
+                <h2>{this.props.post.title}</h2>
+                <div className="post-meta">
+                    By {this.props.post.author} – {this.props.post.created_at}
+                </div>
+            </div>
+        );
+    }
+});
+
+var PostContent = React.createClass({
+    render: function() {
+        return (
+            <div className="post-contents">
+                {this.props.post.contents}
+            </div>
+        );
+    }
+});
