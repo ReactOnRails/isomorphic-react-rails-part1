a9ee79abb419dbe2ca20c25cb6f4a797640588b8
Olivier Lance <olivier@digitalcuisine.fr>
2014-08-27 14:40:43 +0200

    ReactJS gem configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # React production variant (minified)
+  config.react.variant = :production
 end
