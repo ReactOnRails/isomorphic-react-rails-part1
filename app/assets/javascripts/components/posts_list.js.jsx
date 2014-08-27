68bd299e36987e732a19f724093a4f38c4876ad4
Olivier Lance <olivier@digitalcuisine.fr>
2014-08-27 23:48:22 +0200

    The PostsList component

---
+++ app/assets/javascripts/components/posts_list.js.jsx
@@ -0,0 +1,19 @@
+/** @jsx React.DOM **/
+
+var PostsList = React.createClass({
+    getInitialState: function() {
+        return { posts: this.props.initialPosts };
+    },
+
+    render: function() {
+        var posts = this.state.posts.map(function(post) {
+            return <Post key={post.id} post={post} />;
+        });
+
+        return (
+            <div className="posts">
+                {posts}
+            </div>
+        );
+    }
+});
